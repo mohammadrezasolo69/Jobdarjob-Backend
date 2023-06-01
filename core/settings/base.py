3d074a6d15ff7a7c54158427abac64d30141ca47
mohamamdrezasolo <mohamamdrezasolo69@gmail.com>
2023-06-01 21:07:31 +0330

    bugfix

--- core/settings/base.py
+++ core/settings/base.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
 
-    'clickhouse': {
+    'default': {
         'ENGINE': 'clickhouse_backend.backend',
         'NAME': env('NAME_DB'),
         'HOST': env("HOST_DB"),
